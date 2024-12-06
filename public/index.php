c574b95426bac68acc160db81b2d328b451a58fa
rohitramarathinam <rohit.ramarathinam.4@gmail.com>
2024-12-05 20:57:42 -0600

    if logged in redirect straight to home

--- public/index.php
+++ public/index.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
+$is_logged_in = isset($_SESSION['first_name']);
+
+if ($is_logged_in) {
+    header("Location: home.php");
+    exit;
+}
+
 $filePath = dirname(__FILE__) . '/login.html';
 
 if (file_exists($filePath)) {
